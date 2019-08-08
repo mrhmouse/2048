282e7d4a85c4bca4c160f1a6184763e8af768204
Jordan Brown <mrhmouse@gmail.com>
2019-08-08 04:41:26 +0000

    2048: add sliding/combining

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+function padRight(array, length, value) {
+    while (array.length < length)
+	array.push(value)
+    return array
+}
+function padLeft(array, length, value) {
+    while (array.length < length)
+	array.unshift(value)
+    return array
+}
+function rotateRight(grid) {
+    let h = grid.length - 1
+    return grid.map((row, y) => row.map((s, x) => grid[h-x][y]))
+}
+function rotateLeft(grid) {
+    let w = grid[0].length - 1
+    return grid.map((row, y) => row.map((s, x) => grid[x][w-y]))
+}
+
+function isNonZero(s) {
+    return s !== 0
+}
+
 function GameUI(canvas) {
     this.canvas = canvas
     this.ctx = canvas.getContext('2d')
@@ -10,6 +33,54 @@ function GameState(initialState) {
 	 [0, 0, 2, 0],
 	 [0, 0, 0, 0],
 	 [0, 0, 2, 0]]
+}
+GameState.prototype.slideLeft = function() {
+    this.tiles = this.tiles.
+	map(row => padRight(row.filter(isNonZero), row.length, 0))
+}
+GameState.prototype.slideRight = function() {
+    this.tiles = this.tiles.
+	map(row => padLeft(row.filter(isNonZero), row.length, 0))
+}
+GameState.prototype.slideUp = function() {
+    this.tiles = rotateRight(this.tiles)
+    this.slideRight()
+    this.tiles = rotateLeft(this.tiles)
+}
+GameState.prototype.slideDown = function() {
+    this.tiles = rotateRight(this.tiles)
+    this.slideLeft()
+    this.tiles = rotateLeft(this.tiles)
+}
+GameState.prototype.combineLeft = function() {
+    this.tiles.forEach(row => {
+	for (let x = 0; x < row.length-1; ++x) {
+	    if (row[x] === row[x+1]) {
+		row[x] += row[x+1]
+		row[x+1] = 0
+	    }
+	}
+    })
+}
+GameState.prototype.combineRight = function() {
+    this.tiles.forEach(row => {
+	for (let x = row.length-1; x > 0; --x) {
+	    if (row[x] === row[x-1]) {
+		row[x] += row[x-1]
+		row[x-1] = 0
+	    }
+	}
+    })
+}
+GameState.prototype.combineUp = function() {
+    this.tiles = rotateRight(this.tiles)
+    this.combineRight()
+    this.tiles = rotateLeft(this.tiles)
+}
+GameState.prototype.combineDown = function() {
+    this.tiles = rotateRight(this.tiles)
+    this.combineLeft()
+    this.tiles = rotateLeft(this.tiles)
 }
 
 GameUI.prototype.drawState = function(state) {
